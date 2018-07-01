8d78149f67f1b9f88ace1481d738d3d0e22f4140
Cédric Bleschet <cedric.bleschet@gmail.com>
2018-07-01 22:28:26 +0200

    Try to get get piwigo_user working

--- modules/piwigo_user.py
+++ modules/piwigo_user.py
@@ -27,8 +27,9 @@ results:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.urls import open_url
+from ansible.module_utils.urls import fetch_url
 import json
+import base64
 
 
 class PiwigoUserManagement:
@@ -36,47 +37,74 @@ class PiwigoUserManagement:
         self.module = module
 
     # def request_piwigo_login(self):
-    #     # rsp, info = fetch_url(module=self.module, url=self.module.params["url"], data=None)
-    #     # if not rsp or info['status'] >= 400:
-    #     #     self.module.fail_json(msg="failed to connect (status code %s), error was %s" % (
-    #     #     info['status'], info.get('msg', 'no error given')))
-    #
-    #     my_data = { "method": "pwg.session.login", "username": "piwigo_admin", "password": "hfhfhfgf" }
-    #     my_header = {
-    #         'Content-Type': 'application/json',
-    #     }
-    #
-    #     my_url = "http://piwigo.bleschet.fr:8080/ws.php?"
-    #     rsp, info = fetch_url(module=self.module,
-    #                           url=my_url,
-    #                           data=self.module.jsonify(my_data),
-    #                           headers=my_header,
-    #                           method="POST")
-    #
-    #     if info["status"] != 200:
-    #         self.module.fail_json(msg="Failed to  connect to piwigo", response=rsp, info=info)
-    #     # else:
-    #     #     self.module.fail_json(msg="Failed to  connect to piwigo", info=info)
-    #
-    #     # my_url2 = "http://piwigo.bleschet.fr:8080/ws.php?format=json&method=pwg.session.getStatus"
-    #     # rsp2, info2 = fetch_url(module=self.module,
-    #     #                       url=my_url2,
-    #     #                      method="POST")
-    #
-    #     # if info2["status"]:
-    #     #     self.module.fail_json(msg="Failed to  connect to piwigo", response=rsp, info=info)
-    #
-    #     # https://fossies.org/linux/ansible/lib/ansible/module_utils/urls.py
-    #     # https://www.programcreek.com/python/example/99757/ansible.module_utils.urls.fetch_url
-    #     # https://github.com/fraoustin/piwigo
+        # rsp, info = fetch_url(module=self.module, url=self.module.params["url"], data=None)
+        # if not rsp or info['status'] >= 400:
+        #     self.module.fail_json(msg="failed to connect (status code %s), error was %s" % (
+        #     info['status'], info.get('msg', 'no error given')))
+        #
+        # my_data = { "method": "pwg.session.login", "username": "piwigo_admin", "password": "H€2cumzv" }
+        # my_header = {
+        #     'Content-Type': 'application/json',
+        # }
+        #
+        # my_url = "http://piwigo.bleschet.fr:8080/ws.php?format=json&method=pwg.session.login"
+        # rsp, info = fetch_url(module=self.module,
+        #                       url=my_url,
+        #                       data=self.module.jsonify(my_data),
+        #                       headers=my_header,
+        #                       method="POST")
+        #
+        # if info["status"] != 200:
+        #     self.module.fail_json(msg="Failed to  connect to piwigo", response=rsp, info=info)
+        # # else:
+        # #     self.module.fail_json(msg="Failed to  connect to piwigo", info=info)
+        #
+        # # my_url2 = "http://piwigo.bleschet.fr:8080/ws.php?format=json&method=pwg.session.getStatus"
+        # # rsp2, info2 = fetch_url(module=self.module,
+        # #                       url=my_url2,
+        # #                      method="POST")
+        #
+        # # if info2["status"]:
+        # #     self.module.fail_json(msg="Failed to  connect to piwigo", response=rsp, info=info)
+        #
+        # # https://fossies.org/linux/ansible/lib/ansible/module_utils/urls.py
+        # # https://www.programcreek.com/python/example/99757/ansible.module_utils.urls.fetch_url
+        # # https://github.com/fraoustin/piwigo
 
     def request_piwigo_login(self):
-        my_url = "http://piwigo.bleschet.fr:8080/ws.php?&method=pwg.session.login"
-        my_data = {"username": "piwigo_admin", "password": "hfhfhfgf"}
-        rsp = open_url(my_url, self.module.jsonify(my_data), method="POST")
-        rsp_json = json.loads(rsp.read())
-        if rsp_json["total"] == 0:
-            module.fail_json(msg="Environment %s not found." % env_name)
+        # /ws.php?format=json&method=pwg.users.add
+        my_url = "http://piwigo.bleschet.fr:8080/ws.php?format=rest&method=pwg.session.login"
+        my_header = {"Authorization": "Basic %s" % base64.b64encode("piwigo_admin:H€2cumzv")}
+        login_credentials = {
+            'login': {
+
+                'username': 'piwigo_admin',
+                'password': 'H€2cumzv',
+            }
+        }
+        data = 'object=\n%s' % self.module.jsonify(login_credentials)
+
+
+        rsp, info = fetch_url(self.module,
+                              my_url,
+                              data=data,
+                              method="POST")
+
+        if info["status"] != 200:
+            self.module.fail_json(msg="Failed to connect to piwigo", response=rsp, info=info)
+        else:
+            content = rsp.read()
+            if 'stat = "ok"' in content.lower():
+                data = {"response": {"status": "OK"}}
+                self.module.exit_json(msg=data)
+            else:
+                # data = {"response": {"status": "fail", "err": {"msg": content}}}
+                self.module.fail_json(msg=content)
+
+
+        # rsp_json = json.loads(rsp.read())
+        # if rsp_json["total"] == 0:
+        #     module.fail_json(msg="Environment %s not found." % env_name)
 
 def main():
     module = AnsibleModule (
@@ -88,13 +116,13 @@ def main():
             email = dict(required=False, type='str'),
             send_password_by_mail=dict(required=False, default=False, type='bool'),
             url=dict(required=True, type='str'),
-            admin_user=dict(required=True, type='str'),
-            admin_password=dict(required=True, type='str'),
+            url_username=dict(required=True, type='str'),
+            url_password=dict(required=True, type='str'),
         ),
         supports_check_mode=True,
         required_together=[
             ["password", "password_confirm"],
-            ["admin_user", "admin_password"],
+            ["url_username", "url_password"],
         ]
     )
 
