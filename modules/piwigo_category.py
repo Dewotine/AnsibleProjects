9d759bfc1ecc9513d9e75cc53e38f129ed4c1160
Cédric Bleschet <cedric.bleschet@gmail.com>
2018-08-11 19:08:15 +0200

    add category creation script + test

---
+++ modules/piwigo_category.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+DOCUMENTATION='''
+module: piwigo_user
+author: Cédric Bleschet
+description: Module to declare users in piwigo
+options:
+  username:
+    description: User name
+    required: yes
+  password:
+    description:User Password
+    required: false
+'''
+
+EXAMPLES='''
+- name: "Insert Piwigo user"
+  pywigo_user:
+    username: "test"
+    pwg_token: "gdgjdklfjgmfld"
+'''
+
+RETURN = '''
+results:
+    description: user status
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import open_url
+from ansible.module_utils.urls import fetch_url
+import json
+import urllib
+import urllib2
+import base64
+
+class PiwigoCategoryManagement:
+    def __init__(self, module, token, header, ansible_status, api_endpoint):
+        self.module = module
+        self.token = token
+        self.header = header
+        self.ansible_status = ansible_status
+        self.api_endpoint = api_endpoint
+
+    def request_piwigo_login(self):
+        server_name = self.module.params["url"]
+        my_url = server_name + self.api_endpoint
+        my_header = {}
+        values = {'method': 'pwg.session.login',
+                  'username': self.module.params["url_username"],
+                  'password': self.module.params["url_password"]}
+
+        my_data = urllib.urlencode(values)
+        # Get connnexion
+        rsp, info = fetch_url(self.module,
+                              my_url,
+                              data=my_data,
+                              method="POST")
+
+        if info["status"] != 200:
+            self.module.fail_json(msg="Failed to connect to piwigo", response=rsp, info=info)
+        else:
+            content = json.loads(rsp.read())
+            if content['stat'] != "ok":
+                self.module.fail_json(msg=content)
+            else:
+                my_header = {
+                    'Cookie': info['set-cookie']
+                }
+
+        return my_header
+
+    def get_admin_status(self):
+        my_token = ""
+        server_name = self.module.params["url"]
+        my_url = server_name + self.api_endpoint
+        url_method = "&method=pwg.session.getStatus"
+
+        rsp, info = fetch_url(self.module,
+                              my_url + url_method,
+                              headers=self.header,
+                              method="GET")
+        if info["status"] != 200:
+            self.module.fail_json(msg="Failed to get session information from Piwigo", response=rsp, info=info)
+        else:
+            content = json.loads(rsp.read())
+            my_token = content['result']['pwg_token']
+
+        return my_token
+
+    def create_category(self):
+        my_url = self.module.params["url"] + self.api_endpoint
+        values = {'method': 'pwg.categories.add',
+                  'name': self.module.params["name"],
+                  'parent': self.module.params["parent"],
+                  'comment': self.module.params["comment"],
+                  'visible': self.module.params["visible"],
+                  'status':  self.module.params["status"],
+                  'commentable': self.module.params["commentable"],
+                  }
+        my_data = urllib.urlencode(values)
+
+        rsp, info = fetch_url(self.module,
+                              my_url,
+                              data=my_data,
+                              headers=self.header,
+                              method="POST")
+
+        if info["status"] != 200:
+             self.module.fail_json(msg="Failed to connect to piwigo in order to create a category", response=rsp, info=info)
+        else:
+            content = json.loads(rsp.read())
+            if content['stat'] == "ok":
+                setattr(self, 'ansible_status', {'result': 'Changed', 'message':
+                        "Category {0} succesfully added".format(self.module.params["name"])})
+            else:
+                self.module.fail_json(msg="Failed to create Category", response=rsp, info=info)
+
+
+    def finish_request(self):
+        my_url = self.module.params["url"] + self.api_endpoint
+        url_method = "&method=pwg.session.logout"
+
+        fetch_url(self.module,
+                  my_url + url_method,
+                  headers=self.header,
+                  method="GET")
+
+        if self.ansible_status['result'] == 'Changed':
+            self.module.exit_json(changed=True, msg=self.ansible_status['message'])
+        elif self.ansible_status['result'] == 'Unchanged':
+            self.module.exit_json(changed=False, msg=self.ansible_status['message'])
+        else:
+            self.module.fail_json(msg=self.ansible_status['message'])
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            state=dict(type='str', choices=['present', 'absent'], default='present'),
+            name=dict(required=True, type='str'),
+            parent=dict(required=False, type='int', default=0),
+            comment=dict(required=False, default='', type='str'),
+            visible=dict(required=False, default=True, type='bool'),
+            status=dict(type='str', choices=['public', 'private'], default='private'),
+            commentable=dict(required=False, default=True, type='bool'),
+            url = dict(required=True, type='str'),
+            url_username = dict(required=True, type='str'),
+            url_password = dict(required=True, type='str', no_log=True),
+        ),
+        supports_check_mode=True,
+    )
+
+    piwigocategory = PiwigoCategoryManagement(module,
+                                      token="",
+                                      header={'Content-Type': 'application/x-www-form-urlencoded'},
+                                      ansible_status={'result': 'Fail',
+                                                      'message': 'Could not get status of PiwigoCategoryManagement module'},
+                                      api_endpoint="/ws.php?format=json"
+                                      )
+
+    # Get cookie
+    my_header = piwigocategory.request_piwigo_login()
+    setattr(piwigocategory, 'header', my_header)
+
+    # Get token for admin user
+    my_token = piwigocategory.get_admin_status()
+    setattr(piwigocategory, 'token', my_token)
+
+    if module.params['state'] == 'present':
+        piwigocategory.create_category()
+    # elif module.params['state'] == 'absent':
+    #     piwigocategory.delete_user()
+
+    piwigocategory.finish_request()
+
+if __name__ == '__main__':
+    main()
