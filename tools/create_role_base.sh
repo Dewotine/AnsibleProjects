f695333fac1b483aa9d825038cbf242f8e369bb3
Cédric Bleschet <cedric.bleschet@gmail.com>
2019-01-27 14:49:25 +0100

    Add handlers and templates folders

--- tools/create_role_base.sh
+++ tools/create_role_base.sh
@@ -15,9 +15,10 @@ if [ -d "${ROLEDIR}/${1}" ]; then
     exit 2
 fi
 
-mkdir "${ROLEDIR}/${1}" "${ROLEDIR}/${1}/tasks" "${ROLEDIR}/${1}/defaults"
+mkdir "${ROLEDIR}/${1}" "${ROLEDIR}/${1}/tasks" "${ROLEDIR}/${1}/defaults" "${ROLEDIR}/${1}/handlers" "${ROLEDIR}/${1}/templates"
 echo "---" > "${ROLEDIR}/${1}/tasks/main.yml"
 echo "---" > "${ROLEDIR}/${1}/defaults/main.yml"
+echo "---" > "${ROLEDIR}/${1}/handlers/main.yml"
 touch "${ROLEDIR}/${1}/README.md"
 
 git add "${ROLEDIR}/${1}"
